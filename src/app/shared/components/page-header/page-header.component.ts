3044ce9bf221f53b61fec92b22fa04094c4f6817
CristianoSgarbossa <cristianosgarbossadev@gmail.com>
2026-09-18 18:14:04 -0300

    feat: adiciona os componentes compartilhados e o layout da area interna

--- src/app/shared/components/page-header/page-header.component.ts
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -2,6 +2,7 @@ import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-page-header',
+  standalone: true,
   templateUrl: './page-header.component.html',
   styleUrl: './page-header.component.scss'
 })
